5f44db49d012824af92ff482f9551039074e5b59
leonardocanone <leonardocanone@gmail.com>
2023-08-05 10:56:07 -0300

    Primeiro app React criado como prática guiada junto com a instrutora Ana na aula de introdução ao React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
 function App() {
+  const name = "Leo Cannone"
+
+  const olaMundo = () => {
+    alert("Olá, React!")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <button className="meu-nome" onClick={olaMundo}>Clique em min, {name}</button>
+    </>
   );
 }
 
 export default App;
+
+
